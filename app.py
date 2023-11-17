40da10f5b25a97ff303ac43e700b4bdede0758dd
ak-run <123417735+ak-run@users.noreply.github.com>
2023-11-17 11:55:40 +0000

    adding more directories

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm, CSRFProtect
 from wtforms import StringField, SubmitField
 
+
+app = Flask(__name__)
+
+
+if __name__ == '__main__':
+    app.run()
