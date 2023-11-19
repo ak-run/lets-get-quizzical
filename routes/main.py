664de4581ee01af72943343729facf71a4805cd9
Sarah <0osarahtoddo0@gmail.com>
2023-11-19 11:35:39 +0000

    added bootstrap navbar

--- routes/main.py
+++ routes/main.py
@@ -3,12 +3,10 @@ from flask import Blueprint, render_template
 # blueprint for main page
 main_bp = Blueprint("/", __name__, static_folder="static", template_folder="templates")
 
-
 @main_bp.route("/")
 def main():
     """Route for main page"""
     return render_template("index.html")
-
 
 @main_bp.route("/single.html")
 def single():
