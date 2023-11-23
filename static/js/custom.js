42913ef58dda50472b251e906268cb568580fa78
Kamila Pruski <pruskikamila@gmail.com>
2023-11-23 22:54:47 +0000

    fixed problem with next question not displaying properly + added stopwatch class obj to main.py

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,58 +1,56 @@
-let button = document.querySelector('button');
-
 document.addEventListener('DOMContentLoaded', function () {
-	let quizData = {
-		response_code: 0,
-		results: [
-			{
-				question: 'In Mulan (1998), who is the leader of the Huns?',
-				correct_answer: 'Shan Yu',
-				incorrect_answers: ['Chien-Po', 'Li Shang', 'Fa Zhou'],
-			},
-			{
-				question:
-					'Which country do cities of Perth, Adelaide & Brisbane belong to?',
-				correct_answer: 'Australia',
-				incorrect_answers: ['Germany', 'Malta', 'France'],
-			},
-		],
-	};
+    let button = document.querySelector('div.main-button');
+    button.addEventListener('click', nextQuestion);
 
-	let currentQuestionIndex = 0;
+    let quizData = {
+        response_code: 0,
+        results: [
+            {
+                question: 'In Mulan (1998), who is the leader of the Huns?',
+                correct_answer: 'Shan Yu',
+                incorrect_answers: ['Chien-Po', 'Li Shang', 'Fa Zhou'],
+            },
+            {
+                question: 'Which country do cities of Perth, Adelaide & Brisbane belong to?',
+                correct_answer: 'Australia',
+                incorrect_answers: ['Germany', 'Malta', 'France'],
+            },
+        ],
+    };
 
-	function loadQuestion() {
-		let currentQuestion = quizData.results[currentQuestionIndex];
-		document.getElementById('question-number').innerText =
-			currentQuestionIndex + 1;
-		document.getElementById('question-content').innerText =
-			currentQuestion.question;
-		let answersContainer = document.getElementById('answers-container');
-		answersContainer.innerHTML = '';
+    let currentQuestionIndex = 0;
 
-		let allAnswers = [
-			currentQuestion.correct_answer,
-			...currentQuestion.incorrect_answers,
-		];
+    function loadQuestion() {
+        let currentQuestion = quizData.results[currentQuestionIndex];
+        document.getElementById('question-number').innerText =
+            currentQuestionIndex + 1;
+        document.getElementById('question-content').innerText =
+            currentQuestion.question;
+        let answersContainer = document.getElementById('answers-container');
+        answersContainer.innerHTML = '';
 
-		allAnswers.forEach(function (answer) {
-			let answerDiv = document.createElement('div');
-			answerDiv.classList.add('answer');
-			answerDiv.innerHTML = `<input type="radio" name="answer" value="${answer}" class="main-button">
+        let allAnswers = [
+            currentQuestion.correct_answer,
+            ...currentQuestion.incorrect_answers,
+        ];
+
+        allAnswers.forEach(function (answer) {
+            let answerDiv = document.createElement('div');
+            answerDiv.classList.add('answer');
+            answerDiv.innerHTML = `<input type="radio" name="answer" value="${answer}" class="main-button">
                                     <span>${answer}</span>`;
-			answersContainer.appendChild(answerDiv);
-		});
-	}
+            answersContainer.appendChild(answerDiv);
+        });
+    }
 
-	button.addEventListener('click', nextQuestion);
+    function nextQuestion() {
+        currentQuestionIndex++;
+        if (currentQuestionIndex < quizData.results.length) {
+            loadQuestion();
+        } else {
+            alert('Quiz completed!');
+        }
+    }
 
-	function nextQuestion() {
-		currentQuestionIndex++;
-		if (currentQuestionIndex < quizData.results.length) {
-			loadQuestion();
-		} else {
-			alert('Quiz completed!');
-		}
-	}
-
-	loadQuestion();
+    loadQuestion();
 });
