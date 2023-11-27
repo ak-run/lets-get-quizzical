0ed89e428174650caaca9473d8ddf1956db230f9
ak-run <123417735+ak-run@users.noreply.github.com>
2023-11-27 16:03:56 +0000

    get questions in a category

--- models/question_model.py
+++ models/question_model.py
@@ -1,26 +1,28 @@
-# class Question:
-#    pass
-
 import requests
 
 
 class QuizQuestions:
     def __init__(self, category):
-        self.url = # Need to add API url here
-        self.questions_category = category
+        self._url = f"https://the-trivia-api.com/v2/questions?categories={category}"
 
-    def api_call(self, category):
-        # Get data from API
-        response = requests.get(f"{self.url}&category={category}")
-        data = response.json()
+    def get_ten_rand_questions(self):
+        try:
+            response = requests.get(self._url)
+            questions = response.json()
+            if response.status_code != 200:
+                raise ConnectionError
+        except requests.exceptions.RequestException as ce:
+            print('''An error has occurred when trying to
+                  connect to the API: {} '''.format(ce))
+        else:
+            return questions
 
-        if data['response_code'] != 0:
-            exit('Bad API data')
+    # def get_questions(self):
+    #     # Get questions from a specific category
+    #     raw_questions = self.get_ten_rand_questions(self.questions_category)
+    #     return raw_questions
 
-        raw_questions = data['results']
-        return raw_questions
 
-    def get_questions(self):
-        # Get questions from a specific category
-        raw_questions = self.api_call(self.questions_category)
-        return raw_questions
+# quiz = QuizQuestions("music")
+# questions = quiz.get_ten_rand_questions()
+# print(questions)
