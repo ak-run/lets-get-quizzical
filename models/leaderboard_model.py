84c6445d21dafd81c3515f8ba2baec72107e1032
ak-run <123417735+ak-run@users.noreply.github.com>
2023-11-27 11:38:10 +0000

    fixed error in excecuting sql query

--- models/leaderboard_model.py
+++ models/leaderboard_model.py
@@ -11,15 +11,12 @@ class Leaderboard:
         self._display_top10_sql_query = "SELECT position, nickname, score FROM top_scores_view;"
         self._add_user_score_sql_query = "CALL AddUserScore('%s', %s)"
 
-    def execute_sql_query(self, query, *params, fetch_results=False):
-        """Method to execute an SQL query. Optional **param are for queries requiring values being passed"""
+    def execute_sql_query(self, query, fetch_results=False):
+        """Method to execute an SQL query"""
         try:
             with self.db_connection.get_connection_to_db() as connection:
                 cur = connection.cursor()
-                if not params:
-                    cur.execute(query)
-                else:
-                    cur.execute(query, params)
+                cur.execute(query)
 
                 if fetch_results:
                     result = cur.fetchall()
@@ -28,6 +25,7 @@ class Leaderboard:
 
                 connection.commit()
                 return result
+
         except Exception as e:
             connection.rollback()
             raise DbConnectionError(f"Failed to execute query. MySQL Connector Error: {e}")
@@ -38,5 +36,6 @@ class Leaderboard:
 
     def add_user_score(self, nickname, score):
         """Method to add score to database"""
-        result = self.execute_sql_query(self._add_user_score_sql_query, nickname, score)
-        return result, 200
+        formatted_query = self._add_user_score_sql_query % (nickname, score)
+        result = self.execute_sql_query(formatted_query)
+        return result
