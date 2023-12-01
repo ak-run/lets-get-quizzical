2f2091af9bfcb8416af63bb945f937afaa1ef3d4
Charlotte Shorey <charlotte.shorey@outlook.com>
2023-12-01 13:08:31 +0000

    trying to adjust create_quiz_question_dict function to be tested more thoroughly

--- API/API_connection.py
+++ API/API_connection.py
@@ -2,35 +2,66 @@ import requests
 from pprint import pprint as pp
 import random
 
+# API url for quiz questions
+url = "https://the-trivia-api.com/v2/questions"
+
 
 # function to get 10 questions
-def get_ten_rand_questions():
+def get_ten_rand_questions(api_url):
     try:
-        url = "https://the-trivia-api.com/v2/questions"
-        response = requests.get(url)
+        response = requests.get(api_url)
         questions = response.json()
-        if response.status_code != 200:
-            raise ConnectionError
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as http_err:
+        print("HTTP Error: {}".format(http_err))
+        raise SystemExit(http_err)
     except requests.exceptions.RequestException as ce:
+        # this exception covers all potential requesting errors - Timeout, HTTPError, TooManyRedirects
         print('''An error has occurred when trying to
               connect to the API: {} '''.format(ce))
+        raise SystemExit(ce)
     else:
         return questions
 
 
-# pp(get_ten_rand_questions())
+# pp(get_ten_rand_questions(url))
+
+# CORRECT FUNCTION:
+# quiz_questions = []
+# def create_quiz_question_dict(api_result):  # get_ten_rand_questions(url): should be the parameter for function call
+#  # formats the API results. 4 possible answers but correct answer is inserted at a random index 0-3 which is recorded
+#     for raw_question in api_result:
+#         correct_ans_index = random.randint(0, 3)
+#         ans = raw_question['incorrectAnswers']
+#         ans.insert(correct_ans_index, raw_question['correctAnswer'])
+#         quiz_dict = {
+#             'category': raw_question['category'],
+#             'question': raw_question['question']["text"],
+#             'answers': ans,
+#             'correct_answer': correct_ans_index
+#         }
+#         quiz_questions.append(quiz_dict)
+#     return quiz_questions
 
 
-def create_quiz_question_dict():
-    # creates a dict with q's and a's. 4 possible answers but correct answer is given an index 0-3
-    quiz_questions = []
-    for raw_question in get_ten_rand_questions():
-        correct_ans_index = random.randint(0, 3)
-        ans = raw_question['incorrectAnswers']
-        ans.insert(correct_ans_index, raw_question['correctAnswer'])
+# ATTEMPT TO MAKE THE FUNCTION BETTER FORMATTED- to test if the index given actually matches the correct answer
+quiz_questions = []  # the formatted dictionary is appended to this variable
+
+
+def get_rand_index():
+    return random.randint(0, 3)
+
+
+def create_quiz_question_dict(api_result, input_index):
+    """ get_ten_rand_questions(url), and get_random_index() should be the parameters when function is called"""
+    # formats the API results. 4 possible answers but correct answer is inserted at a random index 0-3 which is recorded
+    for question in api_result:
+        correct_ans_index = input_index
+        ans = question['incorrectAnswers']
+        ans.insert(correct_ans_index, question['correctAnswer'])
         quiz_dict = {
-            'category': raw_question['category'],
-            'question': raw_question['question']["text"],
+            'category': question['category'],
+            'question': question['question']["text"],
             'answers': ans,
             'correct_answer': correct_ans_index
         }
@@ -38,4 +69,7 @@ def create_quiz_question_dict():
     return quiz_questions
 
 
-print(create_quiz_question_dict())
+create_quiz_question_dict(get_ten_rand_questions(url), get_rand_index())
+print(quiz_questions)
+
+
--- tests/test_API_connection.py
+++ tests/test_API_connection.py
@@ -3,6 +3,8 @@ import requests
 from API.API_connection import get_ten_rand_questions, create_quiz_question_dict
 
 class TestAPI(unittest.TestCase):
+    """Testing the function get_10_rand_questions is valid, successfully connecting to API and returning
+        a list of 10 dictionaries"""
     url = "https://the-trivia-api.com/v2/questions"
 
     def test_valid_API_connection(self):
@@ -10,26 +12,70 @@ class TestAPI(unittest.TestCase):
         response = requests.get(self.url)
         self.assertEqual(response.status_code, 200)
 
+
     def test_valid_get_ten_rand_questions(self):
         # checking that 10 dictionaries/ questions are returned
-        player_one_q = get_ten_rand_questions()
+        player_one_q = get_ten_rand_questions(api_url="https://the-trivia-api.com/v2/questions")
         count = 0
         for question in player_one_q:
             count += 1
         self.assertEqual(count, 10)
 
-    def test_ConnectionError_is_raised(self):
-        # check ConnectionError raised when status code is not 200
-        pass
-        
+    def test_invalid_url_get_ten_rand_questions(self):
+        # testing assertion raised when the API url is invalid
+        invalid_url = "blah"
+        with self.assertRaises(SystemExit):
+            get_ten_rand_questions(invalid_url)
+
+    def test_HTTPSError_get_ten_rand_questions(self):
+        # testing assertion raised with https error
+        url_not_found = "https://the-trivia-api.com/v2/questionsss"
+        with self.assertRaises(SystemExit):
+            get_ten_rand_questions(url_not_found)
+
+
+class TestCreateQuizQuestionDictFunction(unittest.TestCase):
+    """Testing create_quiz_question_dict function has the correct output"""
+
     def test_valid_create_quiz_question_dict(self):
-        quiz = create_quiz_question_dict()
+        # testing that a list of 10 dictionaries is created
+        url = "https://the-trivia-api.com/v2/questions"
+        result = get_ten_rand_questions(api_url=url)
+        quiz = create_quiz_question_dict(api_result=result)
         count = 0
         for question in quiz:
             count += 1
         self.assertEqual(count, 10)
 
 
-     
+# TEST ATTEMPT
+    def test_correct_answer_from_create_quiz_question_dict(self):
+        # testing that the correct answer is formatted correctly- i.e. given correct index
+        index_input = 1
+        api_input = [{'category': 'arts_and_literature',
+                      'correctAnswer': 'Ivan Turgenev',
+                      'difficulty': 'hard',
+                      'id': '622a1c397cc59eab6f950e61',
+                      'incorrectAnswers': ['Leo Tolstoy', 'Henryk Sienkiewicz', 'Alphonse Daudet'],
+                      'isNiche': False,
+                      'question': {'text': "Which author wrote 'Poems in Prose'?"},
+                      'regions': [],
+                      'tags': ['arts_and_literature'],
+                      'type': 'text_choice'}]
+
+
+
+        expected = {'category': 'arts_and_literature',
+                    'question': "Which author wrote 'Poems in Prose'?",
+                    'answers': ["Leo Tolstoy, "
+                                "Henryk Sienkiewicz",
+                                "Alphonse Daudet's,",
+                                "Ivan Turgenev"],
+                    'correct_answer': "1"}   # THIS IS THE CORRECT ANSWER INDEX
+        result = create_quiz_question_dict(api_input, index_input)
+        self.assertEqual(expected, result)
+
+
+
 if __name__ == '__main__':
     unittest.main()
