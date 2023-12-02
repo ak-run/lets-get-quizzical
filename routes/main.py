9a629236c42fe6ae117530c06ade011fac4a114c
Kamila Pruski <pruskikamila@gmail.com>
2023-12-02 12:20:09 +0000

    changes to main.py - deleting unused notes

--- routes/main.py
+++ routes/main.py
@@ -1,39 +1,8 @@
 from flask import Blueprint, render_template
-from models.stopwatch_model import StopWatch
 
 # blueprint for main page
 main_bp = Blueprint("/", __name__, static_folder="static", template_folder="templates")
 
-
-
-import time
-# def displayMe():
-#     print('let\'s proceed to the next question..')
-#
-# class StopWatch:
-#     def __init__(self, start, end):
-#         self.value = start
-#         self.end = end
-#
-#     def __iter__(self):
-#         return self
-#
-#     def __next__(self):
-#         if self.value < self.end:
-#             raise StopIteration
-#
-#         current = self.value
-#         time.sleep(1)
-#         self.value = self.value - 1
-#
-#         if current == 0:
-#             print('hej, time is up')
-#             displayMe()
-#         return current
-# numbers = StopWatch(60,0)
-# for n in numbers:
-#     print(n)
-#
 
 
 @main_bp.route("/")
