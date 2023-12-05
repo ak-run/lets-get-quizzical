7e2cd9bd3fd6bee8dc590655ace36035ea4c661f
Kamila Pruski <pruskikamila@gmail.com>
2023-12-05 10:33:02 +0000

    changes to main.py, single.html and custom.js - looping in jinja added

--- routes/main.py
+++ routes/main.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from models.question_model import QuizQuestions
 
 # blueprint for main page
 main_bp = Blueprint("/", __name__, static_folder="static", template_folder="templates")
@@ -12,8 +13,10 @@ def main():
 
 @main_bp.route("/single")
 def single():
-    """Route for the single player page"""
-    return render_template("single.html")
+    quiz = QuizQuestions()
+    questions = quiz.create_quiz_question_dict()
+    return render_template("single.html", questions=questions)
+
     
 
 @main_bp.route("/how_to_play")
