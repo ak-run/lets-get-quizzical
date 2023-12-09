6ade2b168ed03c9559830195a3fb210eeac64c31
Sarah <0osarahtoddo0@gmail.com>
2023-12-09 15:02:17 +0000

    add route tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -25,6 +25,39 @@ class TestMainBlueprint(unittest.TestCase):
         self.assertIn(b"<!DOCTYPE html>", response.data)
         self.assertIn(b"<h3>Pub quiz</h3>", response.data)
 
+    def test_valid_how_to_play_route(self):
+        # Test that how to play route returns a 200 status code
+        response = self.client.get('/how_to_play')
+        self.assertEqual(response.status_code, 200)
+
+    def test_valid_how_to_play_route_content(self):
+        # Test the key elements of the content of the how to play route response
+        self.assertIn(b"<!DOCTYPE html>", response.data)
+        self.assertIn(b"<h2>How to Play></h2>", response.data)
+
+    def test_valid_leaderboard_route(self):
+        # Test that Leaderboard route returns a 200 status code
+        response = self.client.get('/leaderboard_main')
+        self.assertEqual(response.status_code, 200)
+
+    def test_valid_leaderboard_route_content(self):
+        # Test the key elements of the content of the leaderboard route response
+        self.assertIn(b"<!DOCTYPE html>", response.data)
+        self.assertIn(b"<h3>Leaderboard</h3>", response.data)
+
+    def test_valid_setup_route(self):
+        # Test that setup route returns a 200 status code
+        response = self.client.get('/setup')
+        self.assertEqual(response.status_code, 200)
+
+    def test_valid_setup_route_content(self):
+        # Test the key elements of the content of the setup route response
+        self.assertIn(b"<!DOCTYPE html>", response.data)
+        self.assertIn(b"<h3>Select category</h3>", response.data)
+
+        
+
+
 
 if __name__ == '__main__':
     unittest.main()
