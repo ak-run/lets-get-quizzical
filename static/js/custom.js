be39c233b66a0c1925d29365656e188a12a111b6
Ruthh88 <ruthhoneywood@gmail.com>
2023-12-10 20:04:43 +0000

    update custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,45 +1,49 @@
 let button = document.querySelector('.next-button');
 let timeDisplay = document.getElementById('time');
-let intervalId
+let intervalId;
+let radioButtons = document.querySelectorAll('input[name="user_answer"]');
+// console.log(radioButtons);
+// let isAnswered = 0
+// let noAnswerError = document.getElementsByClassName('no-answer-error')
+// console.log(noAnswerError);
 
 
 
 let currentQuestionIndex = 0;
 
-
-
-
-// function loadQuestion()
-
-
+if (timeDisplay) {
+    // Only run the stopwatch if timeDisplay exists
+    stopWatch();
+}
 function stopWatch() {
-    let number = 21;
-    intervalId = setInterval(function () {
-        number--;
-        let formattedTime = `00:${String(number).padStart(2, '0')}`;
-        timeDisplay.textContent = formattedTime;
+	let number = 21;
+	intervalId = setInterval(function () {
+		number--;
+		let formattedTime = `00:${String(number).padStart(2, '0')}`;
+		timeDisplay.textContent = formattedTime;
 
-        if (number === 0) {
-            clearInterval(intervalId);
-            currentQuestionIndex++;
-            loadQuestion();
-            timeDisplay.textContent = 'Time is up!';
-            number = 20;
-            stopWatch();
-        }
-    }, 1000);
+		if (number === 0) {
+			clearInterval(intervalId);
+			currentQuestionIndex++;
+			//            loadQuestion();
+			timeDisplay.textContent = 'Time is up!';
+			number = 20;
+			stopWatch();
+		}
+	}, 1000);
 }
 
-//function nextQuestion() {
-//    currentQuestionIndex++;
-//    if (currentQuestionIndex < quizData.results.length) {
-//        clearInterval(intervalId);
-//        // loadQuestion();
-//        stopWatch();
-//    } else {
-//        alert('Quiz completed!');
-//    }
-//}
+ radioButtons.forEach(function(radioButton) {
+     radioButton.addEventListener("click", function(event) {
 
-// loadQuestion()
-stopWatch();
+        button.disabled = false;
+
+
+     });
+ });
+
+
+
+
+
+
