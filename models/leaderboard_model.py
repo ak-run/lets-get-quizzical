f0778648650f3a73070abaeb105e539ae7e413fb
ak-run <123417735+ak-run@users.noreply.github.com>
2023-12-15 09:56:34 +0000

    added docstrings

--- models/leaderboard_model.py
+++ models/leaderboard_model.py
@@ -1,19 +1,27 @@
-import mysql.connector
-
-
 class DbConnectionError(Exception):
+    """Class for DB Connection Exception"""
     pass
 
 
 class Leaderboard:
-    """Class to add new users' score and then display top scores on Leadaerboard"""
+    """Class to add new users' score and then display top scores on Leaderboard"""
     def __init__(self, db_connection):
+        """
+        Initialize the Leaderboard object with a database connection.
+        Parameters: db_connection: An instance of the DatabaseConnection class for database connectivity.
+        """
         self.db_connection = db_connection
         self._display_top10_sql_query = "SELECT position, avatar, nickname, score FROM top_scores_view;"
         self._add_user_score_sql_query = "CALL AddUserScore('%s', '%s', '%s')"
 
     def execute_sql_query(self, query, fetch_results=False):
-        """Method to execute an SQL query"""
+        """
+        Method to execute an SQL query.
+        Parameters: query: The SQL query to be executed.
+                           fetch_results: Flag indicating whether to fetch results after executing the query.
+        Returns: if fetch_results is True, returns a list of results; otherwise, returns None.
+        Raises DbConnectionError: If there is an error executing the query.
+        """
         try:
             with self.db_connection.get_connection_to_db() as connection:
                 cur = connection.cursor()
@@ -32,11 +40,21 @@ class Leaderboard:
             raise DbConnectionError(f"Failed to execute query. MySQL Connector Error: {e}")
 
     def display_top_scores(self):
-        """Method to display leaderboard of top scores"""
+        """
+        Method to display leaderboard of top scores
+        Returns: a list of tuples containing top scores information.
+        """
         return self.execute_sql_query(self._display_top10_sql_query, fetch_results=True)
 
     def add_user_score(self, avatar, nickname, score):
-        """Method to add score to database"""
+        """
+        Method to add a user's score to the database.
+        Parameters:
+            avatar: the path to avatar of the user.
+            nickname: the nickname of the user.
+            score: the score of the user.
+        Returns: result and status_code
+        """
         formatted_query = self._add_user_score_sql_query % (avatar, nickname, score)
         result = self.execute_sql_query(formatted_query)
         return result, 200
