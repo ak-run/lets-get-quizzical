920d1e4bc4464c6ea1815f8be66ae14dd5ba6fd0
ak-run <123417735+ak-run@users.noreply.github.com>
2023-12-15 10:06:07 +0000

    added comments and docstrings

--- models/question_model.py
+++ models/question_model.py
@@ -3,7 +3,12 @@ import requests
 
 
 class QuizQuestions:
-    """Class to make API call and receive 10 random questions in a given category"""
+    """
+    Class to make API call and receive 10 random questions in a given category
+    Attributes:
+        question_categories: A list of available question categories.
+        _url : The base URL for making API calls.
+    """
     def __init__(self):
         self.question_categories = ["music", "sport_and_leisure", "film_and_tv", "arts_and_literature", "history",
                                     "society_and_culture", "science", "geography", "food_and_drink",
@@ -12,18 +17,27 @@ class QuizQuestions:
 
     @property
     def url(self):
+        """Get the current API URL."""
         return self._url
 
     @url.setter
     def url(self, category):
-        """Adding the chosen question category to the API url"""
+        """
+        Set the API URL by adding the chosen question category.
+        Parameters: category: the chosen question category.
+        Raises ValueError if the provided category is invalid.
+        """
         if category in self.question_categories:
             self._url = f"{self._url}?categories={category}"
         else:
             raise ValueError(f"Invalid category: {category}")
 
     def get_ten_rand_questions(self):
-        """For the API call to return 10 questions"""
+        """
+        Make an API call to get 10 random questions.
+        Returns: the JSON response containing 10 random questions.
+        Raises ConnectionError if the API call is unsuccessful.
+        """
         try:
             response = requests.get(self._url)
             questions = response.json()
@@ -36,9 +50,14 @@ class QuizQuestions:
             return questions
 
     def create_quiz_question_dict(self):
-        """Creates a dict with q's and a's. 4 possible answers but correct answer is given an index 0-3"""
+        """
+        Create a dictionary with questions and answers.
+        Returns: a list of dictionaries, each containing a question and its details.
+        """
         quiz_questions = []
         for raw_question in self.get_ten_rand_questions():
+            # generates a random integer between 0 and 3 to determine the index at which the correct answer should
+            # be inserted among the incorrect answers
             correct_ans_index = random.randint(0, 3)
             ans = raw_question['incorrectAnswers']
             ans.insert(correct_ans_index, raw_question['correctAnswer'])
@@ -50,11 +69,3 @@ class QuizQuestions:
             }
             quiz_questions.append(quiz_dict)
         return quiz_questions
-        
-
-    # def get_questions(self):
-    #     # Get questions from a specific category
-    #     raw_questions = self.get_ten_rand_questions(self.questions_category)
-    #     return raw_questions
-    
-
